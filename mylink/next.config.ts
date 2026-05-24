449d93e96b1e87d1aac1467e691fc9a64cee8d9a
cheeseseasoning <cheeseseasoning@users.noreply.github.com>
2026-05-24 19:47:11 +0900

    Configure Github Pages deployment

--- mylink/next.config.ts
+++ mylink/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/my-link",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
